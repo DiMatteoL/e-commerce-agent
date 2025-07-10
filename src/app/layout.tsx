3234b774f1a67ed4441484f5f205ae0ab3595bc7
DiMatteoL <luca7dimatteo@gmail.com>
2025-07-10 14:24:49 +0200

    remove unecessary root layout

--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-// This file is required for Next.js app directory to work
-// The main layout logic is in [locale]/layout.tsx
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
